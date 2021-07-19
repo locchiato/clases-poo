9e280672d236d3ac6334e205ca2139e8a3ada48f
Leandro Occhiato <leandro.occhiato@gmail.com>
2021-07-18 23:51:45 -0300

    [MOD] Se termina la clase Producto.

--- clase20/deTareaCarrito/Producto.java
+++ clase20/deTareaCarrito/Producto.java
@@ -1,4 +1,12 @@
 package clase20.deTareaCarrito;
 
 public class Producto {
+
+    private String descripcion;
+    private Double precio;
+
+    public Producto(String descripcion, Double precio) {
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
 }

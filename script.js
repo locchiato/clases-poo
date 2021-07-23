38cf4d6ec6b6aae2f3caf6e0bfce328b258b4d58
Leandro Occhiato <leandro.occhiato@gmail.com>
2021-07-23 10:39:17 -0300

    [MOD] Ya se puede ver una solucion

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 window.onload = () => {
+    const clase = document.querySelector('h1')
+        .innerText.replace(' ', '').toLowerCase();
 
+    document.querySelector('main').innerHTML += `
+    <img src="${clase}/pg/${clase}.png" />
+    `
 }
